fcdf8781df53f7b5d01754197cb2bbe9764d3879
Lukas Starenko <test.mopnx@passinbox.com>
2025-01-24 17:41:03 +0300

    Add product entity with configuration

---
+++ ProductManager/Data/Configurations/ProductConfig.cs
@@ -0,0 +1,17 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using ProductManager.Entities;
+
+namespace ProductManager.Data.Configurations;
+
+internal class DocumentConfig : IEntityTypeConfiguration<Product>
+{
+    public void Configure(EntityTypeBuilder<Product> builder)
+    {
+        builder.Property(d => d.Name).IsRequired();
+        builder.Property(d => d.ImgUri).IsRequired();
+        builder.Property(d => d.Price).IsRequired().HasPrecision(18, 2);
+        builder.Property(d => d.Description).HasMaxLength(2000);
+    }
+}
