242706e9ad7f11d786dc6a56b3454e6298c2cb46
Lukas Starenko <test.mopnx@passinbox.com>
2025-01-26 09:50:36 +0300

    Add middleware to program cs

--- ProductManager/Program.cs
+++ ProductManager/Program.cs
@@ -6,8 +6,6 @@ using ProductManager.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
@@ -19,15 +17,21 @@ builder.Services.AddTransient<IProductService, ProductService>();
 builder.Services.AddTransient<IApplicationDbContext, ApplicationDbContext>();
 
 builder.Services.AddAutoMapper(typeof(ProductProfile));
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+builder.Services.AddControllers().AddXmlSerializerFormatters();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseDeveloperExceptionPage();
 }
 
 app.UseHttpsRedirection();
+app.MapControllers();
+// app.UseHttpsRedirection();
 
 app.Run();
